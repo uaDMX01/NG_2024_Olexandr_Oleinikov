35e703012eb1533a330cf9331f703778e8dbfc35
uaDMX1 <uagatto@gmail.com>
2024-10-29 09:56:52 +0200

    Created task_5 and push him in GIT

---
+++ Lesson_1/Lesson_5/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hi all!" << endl;
+    return 0;
+}
---
+++ Lesson_1/Task_5/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "hi one!" << endl;
+    return 0;
+}
