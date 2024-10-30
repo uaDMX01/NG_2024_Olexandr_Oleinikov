5d4e00732bea5a98dff370ef706d7a3f7a0cae0c
uaDMX1 <uagatto@gmail.com>
2024-10-30 07:07:47 +0200

    new project start

---
+++ SoloLeveling/LVL_1_1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    const int COUNT_DAYS_IN_WEEK = 7;  // Константа днів тижня
+
+    const char NEW_LINE = '\n';
+    const char TAB = '\t';
+
+    cout <<COUNT_DAYS_IN_WEEK<<NEW_LINE<<TAB<<TAB;
+}
